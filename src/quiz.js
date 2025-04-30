d5f3a12958adefba15e8690d8b4b64e118462761
Barbara <b4b0livei123098@gmail.com>
2025-04-30 20:03:26 -0300

    perguntas

---
+++ src/quiz.js
@@ -0,0 +1,88 @@
+const listaPerguntas = [
+    {
+        pergunta: "Quem transformou a Terra em um planeta governado por insetos gigantes?",
+        primeiraAlternativa: "Summer",
+        segundaAlternativa: "Rick",
+        terceiraAlternativa: "Jerry",
+        quartaAlternativa: "Morty"
+    },
+    {
+        pergunta: "Verdadeiro ou falso: Morty já teve um relacionamento com um robô.",
+        primeiraAlternativa: "Verdadeiro",
+        segundaAlternativa: "Falso",
+        terceiraAlternativa: "Não mostrado na série",
+        quartaAlternativa: "Foi Rick, não Morty"
+    },
+    {
+        pergunta: "Qual personagem teve um surto existencial após descobrir que era um clone",
+        primeiraAlternativa: "Beth",
+        segundaAlternativa: "Summer",
+        terceiraAlternativa: "Morty",
+        quartaAlternativa: "Tammy"
+    },
+    {
+        pergunta: "Quem explodiu a casa da família Smith logo no primeiro episódio?",
+        primeiraAlternativa: " Rick",
+        segundaAlternativa: "Um alienígena",
+        terceiraAlternativa: "Morty",
+        quartaAlternativa: "Jerry"  
+    },
+]
+// Quem transformou a Terra em um planeta governado por insetos gigantes?
+// A) Summer
+// B) Rick
+// C) Jerry
+// D) Morty
+// Resposta certa: B) Rick
+
+// ⸻
+
+// 2. Verdadeiro ou falso: Morty já teve um relacionamento com um robô.
+// A) Verdadeiro
+// B) Falso
+// C) Não mostrado na série
+// D) Foi Rick, não Morty
+// Resposta certa: A) Verdadeiro
+
+// ⸻
+
+// 3. Qual personagem teve um surto existencial após descobrir que era um clone?
+// A) Beth
+// B) Summer
+// C) Morty
+// D) Tammy
+// Resposta certa: A) Beth
+
+// ⸻
+
+// 4. Quem explodiu a casa da família Smith logo no primeiro episódio?
+// A) Rick
+// B) Um alienígena
+// C) Morty
+// D) Jerry
+// Resposta certa: A) Rick
+
+// ⸻
+
+// 5. Verdadeiro ou falso: Rick já se transformou em um picles para evitar uma sessão de terapia.
+// A) Verdadeiro
+// B) Falso
+// C) Ele virou um rato
+// D) Foi Morty quem fez isso
+// Resposta certa: A) Verdadeiro
+// 6. Qual desses personagens foi revelado ser um agente secreto da Federação Galáctica?
+// A) Mr. Poopybutthole
+// B) Birdperson
+// C) Tammy
+// D) Squanchy
+// Resposta certa: C) Tammy
+// 7. Qual é o nome do cientista maluco protagonista da série?
+
+// a) Morty
+
+// b) Rick
+
+// c) Jerry
+
+// d) Summer
+// Resposta: b) Rick

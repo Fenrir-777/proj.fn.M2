da4c557765efcfe7a90ef90e73ab1a4a54d7dd27
daviflavio555@gmail.com <daviflavio555@gmail.com>
2025-05-06 20:28:14 -0300

    Ajustes

--- src/inicio.js
+++ src/inicio.js
@@ -1,11 +1,9 @@
 function nomePagina() {
-    const nome=
-    document.getElementByld("nome");
-    let nomeUsuario=
-    prompt("Digite seu nome");
-    nome.innerText= `Seja bem vindo ${nomeUsuario}`;
+    const nome = document.getElementByld("nome");
+    let nomeUsuario = prompt("Digite seu nome");
+    nome.innerText = `Seja bem vindo ${nomeUsuario}`;
 }
-nomePagina()
+nomePagina();
 
 
 function trocarQuiz() {
@@ -14,5 +12,4 @@ function trocarQuiz() {
         window.location.href = "quiz.html";
     });
 }
-
 trocarQuiz();
